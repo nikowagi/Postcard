073548174aa158b75973954cf43ad121278ae920
nikowagi <dorkfromfl@MacBook.local>
2015-05-10 18:02:24 -0700

    added new features with the name section

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -29,16 +30,33 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton){
         //evaluates button
+        //hide labels
         messageLabel.hidden = false
+        nameLabel.hidden = false
+        
+        //name label text
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        //message label text
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
         
-        
+        //hides keyboard
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        
+        
+        
+        //changes the button to send mail to mail sent
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
         
     }
     
